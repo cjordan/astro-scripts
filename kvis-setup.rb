c00776f30aa8cc41d3b1fcd39bf2bd82ddb7b1a2
Christopher Jordan <christopherjordan87@gmail.com>
2013-12-31 15:44:39 +1000

    Made each window a separate class - much tidier.

--- kvis-setup.rb
+++ kvis-setup.rb
@@ -18,14 +18,191 @@ OptionParser.new do |opts|
     $sleep_time = 0.05
 	opts.on('-s','--sleep_time NUM','Specify the amount of time to sleep between actions. May be useful for slow computers or networks. Default is 0.05s.') {|o| $sleep_time = o.to_f}
 
-    options[:layout] = "square"
+    options[:layout] = "wide"
     opts.on('-l','--layout STR','Specify the layout. Supported orientations are:
                                      - \'square\'
                                      - \'wide\'
-                                     Default is \'square\'') {|o| options[:layout] = o}
+                                     Default is \'wide\'') {|o| options[:layout] = o}
 
     opts.on('-d','--debug','Prints the commands being sent to xdotool.') {$debug = true}
 end.parse!
+
+
+## Common "Window" class - all kvis windows inherit these properties
+class Window
+    # Pass in the window id so we know which window to work upon
+    def initialize(id)
+        @id = id
+        # Resize the window to "default" if it is not already this size
+        xdotool "windowsize --sync #{@id} #{@default.join(' ')}" unless get_geometry(@id) == @default
+    end
+    # Raise this window above all others (will not work for all window managers)
+    def raise
+        xdotool "windowraise #{@id}"
+    end
+    # Move this window to new coordinates (x,y)
+    def move(x,y)
+        xdotool "windowmove --sync #{@id} #{x} #{y}"
+    end
+    # Resize this window to geometry (x,y)
+    def size(x,y)
+        xdotool "windowsize --sync #{@id} #{x} #{y}"
+    end
+    # Closes the current window
+    # Use with care - the coordinates of the button must be specified per window.
+    # If this is run without "close" properly defined, the script will exit prematurely.
+    def close
+        abort("#{File.basename(__FILE__)}: Window #{@id} did not close - your settings may need adjusting. Exiting...") if win_is_open?(@id)
+    end
+end
+
+## Primary kvis window
+class Kvis < Window
+    def initialize(id)
+        # Default window size. Resize if necessary.
+        @default = [522,614]
+        super
+    end
+    # Open the Files window
+    def files
+        click_on(@id,30,15)
+    end
+    # Opens up an element from the Intensity menu
+    def intensity(element)
+        case element.downcase
+        when "pseudo"
+            navigate_dropdown(@id,100,15,100,65)
+        end
+    end
+    # Opens up an element from the Overlay menu
+    def overlay(element)
+        case element.downcase
+        when "axis"
+            navigate_dropdown(@id,230,15,230,65)
+        when "annotation"
+            navigate_dropdown(@id,230,15,230,160)
+        end
+    end
+    # Open the View window
+    def view
+        click_on(@id,365,15)
+    end
+end
+
+## Browser window
+class Browser < Window
+    def initialize(id)
+        @default = [439,588]
+        super
+    end
+    def close
+        click_on(@id,30,15)
+        super
+    end
+end
+
+## Axis Labels window
+class Axis < Window
+    def initialize(id)
+        @default = [344,331]
+        super
+    end
+    def close
+        click_on(@id,30,15)
+        super
+    end
+    def enable
+        click_on(@id,100,15)
+    end
+    def paper_colours
+        click_on(@id,200,140)
+    end
+end
+
+## PseudoColour window
+class Pseudo < Window
+    def initialize(id)
+        @default = [418,393]
+        super
+    end
+    def close
+        click_on(@id,30,15)
+        super
+    end
+    def reverse
+        click_on(@id,130,15)
+    end
+    def glynn_rogers2
+        click_on(@id,230,295)
+    end
+end
+
+## View window
+class View < Window
+    def initialize(id)
+        @default = [460,249]
+        super
+    end
+    def close
+        click_on(@id,30,15)
+        super
+    end
+    def marker
+        click_on(@id,100,115)
+    end
+    def profile(element)
+        case element.downcase
+        when "line"
+            navigate_dropdown(@id,200,40,200,90)
+        when "box_sum"
+            navigate_dropdown(@id,200,40,200,115)
+        end
+    end
+end
+
+## Profile window
+class Profile < Window
+    def initialize(id)
+        @default = [442,436]
+        super
+    end
+    def close
+        click_on(@id,30,15)
+        super
+    end
+    def v_zoom
+        click_on(@id,100,15)
+    end
+    def style(element)
+        case element.downcase
+        when "hist"
+            navigate_dropdown(@id,280,40,280,90)
+        end
+    end
+    def overlay(element)
+        case element.downcase
+        when "axis"
+            navigate_dropdown(@id,100,40,100,90)
+        end
+    end
+end
+
+## Files window
+class Files < Window
+    def initialize(id)
+        # The Files window is special - it will size itself according to the files in the pwd.
+        # Resize it here so the button placement is predictable.
+        @default = [400,400]
+        super
+    end
+    def close
+        click_on(@id,30,360)
+        super
+    end
+    def pin
+        click_on(@id,220,360)
+    end
+end
 
 
 def xdotool(command)
@@ -34,45 +211,28 @@ def xdotool(command)
     # "strip" is necessary for pesky newlines
     `xdotool #{command}`.strip
 end
-
 def get_window_id(search_string)
-    xdotool "search --name '#{search_string}'"
+    (xdotool "search --name '#{search_string}'").split("\n")[-1]
 end
-
-def get_geometry(id)
-    # xdotool does not report the position of windows correctly - use xwininfo instead
-    (`xwininfo -id #{id}`).scan(/Absolute.*:\s*(\d*)/).flatten.map{|n| n.to_i}
-end
-
-def get_win_decorator_height(id)
-    output = (`xwininfo -id #{id}`).scan(/^.*Y:\s*(\d*)/).flatten.map{|n| n.to_i}
-    return output[0] - output[1]
-end
-
-def get_top_bar_height(id)
-    (`xwininfo -id #{id}`).scan(/Relative.*Y:\s*(\d*)/)[0][0].to_i
-end
-
 def click_on(id,x,y)
-    # Get the geometry of this window id
-    geometry = get_geometry(id)
-    # Add the [x,y] passed in by geometry to x1, y1, x2, y2
-    x += geometry[0]
-    y += geometry[1]
-    # Move the mouse to x,y, then click
+    # Get the position of this window id
+    position = get_position(id)
+    # Add the [x,y] passed in by get_position to our x and y
+    x += position[0]
+    y += position[1]
+    # Move the mouse to (x,y), then click
     xdotool "mousemove #{x} #{y}"
     xdotool "click 1"
     sleep $sleep_time
 end
-
 def navigate_dropdown(id,x1,y1,x2,y2)
-    # Get the geometry of this window id
-    geometry = get_geometry(id)
-    # Add the [x,y] passed in by geometry to x1, y1, x2, y2
-    x1 += geometry[0]
-    y1 += geometry[1]
-    x2 += geometry[0]
-    y2 += geometry[1]
+    # Get the position of this window id
+    position = get_position(id)
+    # Add the [x,y] passed in by position to x1, y1, x2, y2
+    x1 += position[0]
+    y1 += position[1]
+    x2 += position[0]
+    y2 += position[1]
     # It's ugly, but "moving" the mouse on and off the dropdown button works better
     xdotool "mousemove #{x1} #{y1}"
     xdotool "mousemove #{x1} #{y1-30}"
@@ -84,131 +244,142 @@ def navigate_dropdown(id,x1,y1,x2,y2)
     sleep $sleep_time
 end
 
-
-## Get some information on the desktop so we can place windows neatly at the edges
-# Record the mouse position so we can reset it when done
-original_mouse_pos = (xdotool "getmouselocation").scan(/:(\d*)/)[0..1].join(' ')
-# Move the mouse to somewhere very far away...
-xdotool "mousemove 5000 5000"
-# The mouse will only move as far away as the desktop allows, so now we have the screen geometry
-screen_geometry = (xdotool "getmouselocation").scan(/:(\d*)/)[0..1].flatten.map{|n| n.to_i}
-
-# Find the main kvis window and select it
-kvis_id = get_window_id("kvis.*Karma")
-# Raise the window
-# This will not work on all window managers - however, all is fine if kvis is on top
-xdotool "windowraise #{kvis_id}"
-
-# Get the window decorator height
-win_dec_height = get_win_decorator_height(kvis_id)
-# Window manager top bar height
-top_bar_height = get_top_bar_height(kvis_id)
-
-
-## Window formatting variables
-# Array browser (sitting on the right)
-browser_width = 500
-browser_height = 600
-
-# File browser (sitting on the left)
-file_width = 500
-file_height = screen_geometry[1] - 2*top_bar_height - win_dec_height - 2
-
-# Main kvis window - "profile" selection
-case options[:layout]
-when "wide"
-    kvis_width = 1400
-    kvis_height = 700
-    kvis_pos_x = 0
-    kvis_pos_y = top_bar_height
-else
-    puts "*** kvis-setup.rb: Layout specified is not recognised; defaulting to 'square'." unless options[:layout] == "square"
-    kvis_width = 800
-    kvis_height = 800
-    kvis_pos_x = file_width+2
-    kvis_pos_y = top_bar_height
+def get_position(id)
+    (`xwininfo -id #{id}`).scan(/Absolute.*:\s*(\d*)/).flatten.map{|n| n.to_i}
+end
+def get_geometry(id)
+    (`xwininfo -id #{id}`).scan(/(Width|Height):\s*(\d*)/).map{|n| n[1].to_i}
+end
+def get_win_decorator_height(id)
+    (`xwininfo -id #{id}`).scan(/^.*Y:\s*(\d*)/).flatten.map{|n| n.to_i}.reduce(:-)
+end
+def get_top_bar_height(id)
+    (`xwininfo -id #{id}`).scan(/Relative.*Y:\s*(\d*)/)[0][0].to_i
+end
+def win_is_open?(id)
+    (`xwininfo -id #{id}`).match(/Map State:\s*(IsViewable)/) ? true : false
 end
 
 
-## Axis labels and Paper colours
-# Open "Axis labels" from the "Overlay" dialog...
-navigate_dropdown(kvis_id,230,15,230,65)
-# Find the "Axis labels" window
-axis_id = get_window_id("dressingControlPopup")
-# Enable axis labels
-click_on(axis_id,100,15)
-# Enable "Paper Colours"
-click_on(axis_id,200,140)
-# ... and close the dialog
-click_on(axis_id,30,15)
+# Record the mouse position so we can reset it when done
+original_mouse_pos = (xdotool "getmouselocation").scan(/:(\d*)/)[0..1].join(' ')
+
+# Get the desktop geometry so we can place windows neatly at the edges
+screen_geometry = (`xwininfo -root`).scan(/(Width|Height):\s*(\d*)/).map{|n| n[1].to_i}
 
 
-## Colour scheme
-# Open "PseudoColour" from the "Intensity" dialog...
-navigate_dropdown(kvis_id,100,15,100,65)
-# Find the "PseudoColour" window
-pseudo_id = get_window_id("pseudoCmapwinpopup")
-# Disable "Reverse" from "Paper Colours"
-click_on(pseudo_id,130,15)
-# Use the "Glynn Rogers 2" profile
-click_on(pseudo_id,230,295)
-# ... and close the dialog
-click_on(pseudo_id,30,15)
+## Window manipulation
+# From the main kvis window, open the Axis Labels window
+kvis = Kvis.new(get_window_id("kvis.*Karma"))
+kvis.raise
+kvis.overlay("axis")
+
+# Set axis labels to be enabled, along with paper colours
+axis = Axis.new(get_window_id("dressingControlPopup"))
+axis.enable
+axis.paper_colours
+axis.close
+
+# Set the colour scale to by "Glynn Rogers2", and disable the "Reverse" option
+kvis.intensity("pseudo")
+pseudo = Pseudo.new(get_window_id("pseudoCmapwinpopup"))
+pseudo.reverse
+pseudo.glynn_rogers2
+pseudo.close
+
+# Open the View window and enable "Show Marker in Line Profile"
+kvis.view
+view = View.new(get_window_id("View Control for display window"))
+view.marker
+# Open the "Box Sum" profile
+view.profile("box_sum")
+
+# Enable "Auto V Zoom" and set the "Style" to "hist"
+profile = Profile.new(get_window_id("Profile Window for display window"))
+profile.v_zoom
+profile.style("hist")
+# Open the Axis Labels window for the profile window
+profile.overlay("axis")
+
+# Enable Axis Labels and paper colours
+axis = Axis.new(get_window_id("dressingControlPopup"))
+axis.enable
+axis.paper_colours
+axis.close
+
+# Close the View window
+view.raise
+# *** This is a workaround until I can get dwm to play nicely with raising windows
+view.move(screen_geometry[0]-50, 0)
+view.close
+# *** This is a workaround until I can get dwm to play nicely with raising windows
+profile.move(screen_geometry[0]-50, screen_geometry[1]-50)
+
+# Open the Files window
+kvis.raise
+kvis.files
+files = Files.new(get_window_id("Array File Selector"))
+# Set the Pin option
+files.pin
 
 
-## Spectrum "profile" window
-# Open the "View" dialog...
-click_on(kvis_id,365,15)
-# Find the "View" window
-view_id = get_window_id("View Control for display window")
-# Enable "Show Marker in Line Profile"
-click_on(view_id,100,115)
-# Select "Box Sum" from the "Profile Mode" list...
-navigate_dropdown(view_id,200,40,200,115)
-# Find the "Profile" window
-profile_id = get_window_id("Profile Window for display window")
-# Enable "Auto V Zoom"
-click_on(profile_id,100,15)
-# Make the style "hist"
-navigate_dropdown(profile_id,280,40,280,90)
-# Open the "Overlay dialog" and make the profile window have paper colours
-navigate_dropdown(profile_id,100,40,100,90)
-# Find the "Axis labels" window
-# Unfortunately, by default this will find the previous "Axis" window, even though it's closed
-axis2_id = get_window_id("dressingControlPopup").split("\n")[1]
-# Enable axis labels
-click_on(axis2_id,100,15)
-# Enable "Paper Colours"
-click_on(axis2_id,200,140)
-# ... and close the dialog
-click_on(axis2_id,30,15)
-# Move the profile window and resize it
-xdotool "windowsize --sync #{profile_id} #{browser_width} #{screen_geometry[1] - 2*top_bar_height - browser_height - 2*win_dec_height - 4}"
-xdotool "windowmove #{profile_id} #{screen_geometry[0]-browser_width-2} #{top_bar_height + browser_height + 2*win_dec_height + 2}"
-# and close the "View" dialog
-click_on(view_id,30,15)
+## Window formatting variables
+kvis_id = get_window_id("kvis.*Karma")
+# Window decorator height
+win_dec_height = get_win_decorator_height(kvis_id)
+# Window manager top bar height - this assumes the main kvis window is positioned just beneath it
+top_bar_height = get_top_bar_height(kvis_id)
+
+# Files window (sitting on the left)
+files_width = 500
+files_height = screen_geometry[1] - 2*top_bar_height - win_dec_height - 4
+files_x = 0
+files_y = top_bar_height
+
+# Main kvis window - "profile" selection
+case options[:layout]
+when "square"
+    kvis_width = 800
+    kvis_height = 800
+    kvis_x = files_width + 2
+    kvis_y = top_bar_height
+else
+    puts "*** kvis-setup.rb: Layout specified is not recognised; defaulting to 'wide'." unless options[:layout] == "wide"
+    kvis_width = 1400
+    kvis_height = 700
+    kvis_x = 0
+    kvis_y = top_bar_height
+end
+
+# Browser (sitting on the right)
+browser_width = 500
+browser_height = 600
+browser_x = screen_geometry[0] - browser_width - 4
+browser_y = top_bar_height
+
+# Profile window (sitting on the right)
+profile_width = browser_width
+profile_height = screen_geometry[1] - 2*top_bar_height - browser_height - 2*win_dec_height - 6
+profile_x = screen_geometry[0] - browser_width - 4
+profile_y = top_bar_height + browser_height + 2*win_dec_height + 2
 
 
-# Open the "Files" dialog and resize it
-# Don't know why, but this needs to be done twice.
-click_on(kvis_id,30,15)
-click_on(kvis_id,30,15)
-# Find the "Files" window
-files_id = get_window_id("Array File Selector")
-# Enable the pin option
-click_on(files_id,225,965)
-# Resize it
-xdotool "windowsize --sync #{files_id} #{file_width} #{file_height}"
+## Move and resize all the windows
+kvis.size(kvis_width, kvis_height)
+kvis.move(kvis_x, kvis_y)
+
+files.size(files_width, files_height)
+files.move(files_x, files_y)
+
+browser = Browser.new(get_window_id("Browser.*for display window"))
+browser.size(browser_width, browser_height)
+browser.move(browser_x, browser_y)
+
+profile.size(profile_width, profile_height)
+profile.move(profile_x, profile_y)
 
 
-# Move and resize the browser window
-browser_id = get_window_id("Browser.*for display window")
-xdotool "windowsize --sync #{browser_id} #{browser_width} #{browser_height}"
-xdotool "windowmove --sync #{browser_id} #{screen_geometry[0]-browser_width-2} #{top_bar_height}"
-
-# Move and resize the main kvis window
-xdotool "windowsize --sync #{kvis_id} #{kvis_width} #{kvis_height}"
-xdotool "windowmove --sync #{kvis_id} #{kvis_pos_x} #{kvis_pos_y}"
-
-# Return the mouse to where we started
+## Return the mouse to where we started
 xdotool "mousemove #{original_mouse_pos}"
+
+## Done!
